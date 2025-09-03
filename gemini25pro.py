129dcf133006d8aa278c27c9a2d4df34715135e5
ShubhamBhawsar-GVW <shubham.b@global-value-web.com>
2025-09-03 14:54:24 +0530

    Update gemini25pro.py
    update "app.run(host="0.0.0.0", port=5002, debug=False)"

--- gemini25pro.py
+++ gemini25pro.py
@@ -390,7 +390,7 @@ def transcribe_and_translate():
         }, ensure_ascii=False, indent=2), 500, {'Content-Type': 'application/json; charset=utf-8'}
 
 if __name__ == "__main__":
-    app.run(port=5002, debug=False, host='127.0.0.1')
+    app.run(host="0.0.0.0", port=5002, debug=False)
 
 
 
